d71726d36cac35e8ee90e00e352328e7aaff04fc
AbdelrhmanAhmedA3 <abdelrhmanahmed9696@gmail.com>
2025-07-21 18:29:12 +0300

    add code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/index').then((m) => m.Home),
+  },
+  {
+    path: 'group',
+    loadComponent: () => import('./pages/index').then((m) => m.Group),
+  },
+];
